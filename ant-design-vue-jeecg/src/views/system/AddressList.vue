fa3b880d924c724e7c64d76c960b64d4e0f0c0a0
honghonggan <2941567857@qq.com>
2024-12-23 21:22:06 +0800

    首次git改动

--- ant-design-vue-jeecg/src/views/system/AddressList.vue
+++ ant-design-vue-jeecg/src/views/system/AddressList.vue
@@ -18,7 +18,7 @@
     components: { AddressListLeft, AddressListRight },
     data() {
       return {
-        description: '通讯录页面',
+        description: '通讯录页面首次git修改',
         currentOrgCode: ''
       }
     },
